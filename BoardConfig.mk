c244030c7c543d14570149a2cba4add2843f8b31
ata-kaner <ata_kaner@outlook.com>
2023-11-24 09:34:59 +0300

    r8q: Add custom mkbootimg
    From TeamWin/android_device_samsung_a52sxq

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,9 @@ TARGET_USES_UEFI := true
 # Assert
 TARGET_OTA_ASSERT_DEVICE := r8q
 
+# Kernel
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
+
 # Kernel: Base flags
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
